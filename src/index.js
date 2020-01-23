325ed41837287a513d26d604449d7a3f5d2c5b2a
Damjan Pavlica <damjan.pavlica@origo.is>
2020-01-23 13:38:56 +0100

    css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { HashRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import './style.css'
 
 import {store} from './store'
 import App from './components/App'

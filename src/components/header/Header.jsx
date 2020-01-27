b2c130e188bacd539e53a4ce46edb3dcf29ed61f
Damjan Pavlica <damjan.pavlica@origo.is>
2020-01-27 15:52:24 +0100

    pomalo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
   return (
     <header id="header">
       <div className='site-info'>
-        <h3>{translate('APP_NAME')} <span role="img" aria-label="vatra">🔥</span></h3> <small className='info-text'>{translate('HEADER_TEXT')}</small>
+        <h3>{translate('APP_NAME')}</h3> <small className='info-text'>❁ {translate('HEADER_TEXT')}</small>
       </div>
       <div className="header-main">
         <nav>

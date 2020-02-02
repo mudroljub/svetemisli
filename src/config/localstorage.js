4b7f7c174354fb4cf3c34e86f06cbe1b6dca2864
Damjan Pavlica <mudroljub@gmail.com>
2020-02-02 21:15:36 +0100

    delete all

--- src/config/localstorage.js
+++
@@ -1,8 +0,0 @@
-export const LS = {
-  service: 'sveteMisli.authService',
-  token: 'sveteMisli.token',
-  lang: 'sveteMisli.lang',
-  script: 'sveteMisli.script',
-  translationMode: 'sveteMisli.translationMode',
-  devMode: 'sveteMisli.devMode',
-}

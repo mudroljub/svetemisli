7ad7a1e1d236e07b0782618a483aadc9ce5f2f92
Damjan Pavlica <damjan.pavlica@origo.is>
2020-02-04 09:41:29 +0100

    to svetemysli-api

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 export const domain = process.env.REACT_APP_ENV === 'development'
   ? 'http://localhost:5000'
-  : 'https://programming-quotes-api.herokuapp.com'
+  : 'https://svetemysli-api.herokuapp.com'
 
 export const API = {
   create: `${domain}/svetemisli/`,

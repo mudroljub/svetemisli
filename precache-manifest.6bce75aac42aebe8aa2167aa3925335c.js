b9266e5c311d5c88cdaa58566aa66d111cb68b97
Damjan Pavlica <damjan.pavlica@origo.is>
2020-02-06 16:13:05 +0100

    Updates

---
+++ precache-manifest.6bce75aac42aebe8aa2167aa3925335c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "757b3e10fd841b4c77fd2d94445b3188",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10f8ab26bdaebe4ece8e",
+    "url": "/static/css/main.915375bd.chunk.css"
+  },
+  {
+    "revision": "9738bda9677aa52689a8",
+    "url": "/static/js/2.1401db52.chunk.js"
+  },
+  {
+    "revision": "4cdd2411cb4fa1c1c4ef19dc4dc1d081",
+    "url": "/static/js/2.1401db52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10f8ab26bdaebe4ece8e",
+    "url": "/static/js/main.bab68288.chunk.js"
+  },
+  {
+    "revision": "db4e959bd83a7adf2f61",
+    "url": "/static/js/runtime-main.e06f0d8c.js"
+  },
+  {
+    "revision": "c4adc8f0d17d3d7f058c53f5de0ff581",
+    "url": "/static/media/chakra.c4adc8f0.svg"
+  },
+  {
+    "revision": "5c66df997e400329178124512317e569",
+    "url": "/static/media/papir.5c66df99.webp"
+  },
+  {
+    "revision": "8c6459b9814e1ce6247095ce40bed1ee",
+    "url": "/static/media/preloader.8c6459b9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.28bf14f556c673f2d8bca7f0b8f699f4.js"
+  "/precache-manifest.6bce75aac42aebe8aa2167aa3925335c.js"
 );
 
 self.addEventListener('message', (event) => {

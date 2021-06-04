84a0cc59208f9bb82d4d2c51738fed4083c6e8bc
Damjan Pavlica <damjan.pavlica@origo.is>
2021-06-04 08:55:26 +0200

    m

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -5,8 +5,6 @@ const storage = localStorage.getItem('sveteMisli')
   ? JSON.parse(localStorage.getItem('sveteMisli'))
   : {}
 
-console.log(storage)
-
 const allQuotes = storage.allQuotes || shuffle(quotes)
 const lang = storage.lang || 'ms'
 
@@ -21,8 +19,8 @@ export default {
   maxLength,
   lang,
   script: storage.script || 'kir',
-  showFilters: storage.showFilters === 'true',
-  showSidebar: storage.showSidebar === 'true',
+  showFilters: storage.showFilters,
+  showSidebar: storage.showSidebar,
   minLimit: minLength,
   maxLimit: maxLength,
   selectedAuthors: new Set(), // selected from sidebar

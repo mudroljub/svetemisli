646cd5a5de3f4d35faf3f0145eaa9dcb0c050e86
Damjan Pavlica <damjan.pavlica@origo.is>
2021-06-08 18:00:32 +0200

    localStorage

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -5,10 +5,19 @@ const storage = localStorage.getItem('sveteMisli')
   ? JSON.parse(localStorage.getItem('sveteMisli'))
   : {}
 
+const {script, minLimit, maxLimit, showFilters, showSidebar} = storage
 const allQuotes = shuffle(storage.allQuotes || quotes)
 const lang = storage.lang || 'ms'
 
-const {minLength, maxLength, allAuthors, filteredQuotes, filteredAuthors} = getDerived(allQuotes, lang)
+const filter = q =>
+  q[lang]
+  && q[lang].length >= minLimit && q[lang].length <= maxLimit
+
+const derived = (minLimit && maxLimit)
+  ? getDerived(allQuotes, lang, filter)
+  : getDerived(allQuotes, lang)
+
+const {minLength, maxLength, allAuthors, filteredQuotes, filteredAuthors} = derived
 
 export default {
   allQuotes,
@@ -18,11 +27,11 @@ export default {
   minLength,
   maxLength,
   lang,
-  script: storage.script || 'kir',
-  showFilters: storage.showFilters,
-  showSidebar: storage.showSidebar,
-  minLimit: storage.minLimit || minLength,
-  maxLimit: storage.maxLimit || maxLength,
+  script: script || 'kir',
+  showFilters,
+  showSidebar,
+  minLimit: minLimit || minLength,
+  maxLimit: maxLimit || maxLength,
   selectedAuthors: new Set(),
   phrase: '',
   authorPhrase: '',

0d3e0a9803588cf11c8134b997817bd069ac518d
Damjan Pavlica <damjan.pavlica@origo.is>
2021-06-12 01:22:27 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,45 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+// https://googlechrome.github.io/samples/service-worker/basic/
+const PRECACHE = 'precache-v8'
+const RUNTIME = 'runtime'
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+  '/', // Alias for index.html
+  'index.html',
+]
 
-importScripts(
-  "/precache-manifest.6ebdc52a933ad7a2e012b51afc1d39a1.js"
-);
+// The install handler takes care of precaching the resources we always need.
+window.self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(window.self.skipWaiting())
+  )
+})
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+// The activate handler takes care of cleaning up old caches.
+window.self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME]
+  event.waitUntil(
+    caches.keys().then(cacheNames => cacheNames.filter(cacheName => !currentCaches.includes(cacheName))).then(cachesToDelete => Promise.all(cachesToDelete.map(cacheToDelete => caches.delete(cacheToDelete)))).then(() => window.self.clients.claim())
+  )
+})
 
-workbox.core.clientsClaim();
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+window.self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(window.self.location.origin))
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse)
+          return cachedResponse
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
-});
+        return caches.open(RUNTIME).then(cache => fetch(event.request).then(response =>
+        // Put a copy of the response in the runtime cache.
+          cache.put(event.request, response.clone()).then(() => response)
+        ))
+      })
+    )
+})

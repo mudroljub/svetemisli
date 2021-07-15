56ad56906e309750b4c5996685f50a075cdde60c
Damjan Pavlica <damjan.pavlica@origo.is>
2021-07-15 20:55:08 +0200

    refactor latinicDict

--- src/utils/transliterate.js
+++ src/utils/transliterate.js
@@ -17,72 +17,41 @@ const msSpecific = text => text
   .replace(/Nj/g, 'Нь')
   .replace(/NJ/g, 'Нь')
 
-const toCyrillic = text => text
-  .replace(/a/g, 'а')
-  .replace(/b/g, 'б')
-  .replace(/c/g, 'ц')
-  .replace(/č/g, 'ч')
-  .replace(/ć/g, 'ћ')
-  .replace(/d/g, 'д')
-  .replace(/đ/g, 'ђ')
-  .replace(/e/g, 'е')
-  .replace(/ě/g, 'ѣ')
-  .replace(/f/g, 'ф')
-  .replace(/g/g, 'г')
-  .replace(/h/g, 'х')
-  .replace(/i/g, 'и')
-  .replace(/y/g, 'ы')
-  .replace(/j/g, 'ј')
-  .replace(/k/g, 'к')
-  .replace(/l/g, 'л')
-  .replace(/m/g, 'м')
-  .replace(/n/g, 'н')
-  .replace(/o/g, 'о')
-  .replace(/p/g, 'п')
-  .replace(/r/g, 'р')
-  .replace(/s/g, 'с')
-  .replace(/ś/g, 'сь')
-  .replace(/š/g, 'ш')
-  .replace(/t/g, 'т')
-  .replace(/u/g, 'у')
-  .replace(/v/g, 'в')
-  .replace(/z/g, 'з')
-  .replace(/ź/g, 'зь')
-  .replace(/ž/g, 'ж')
+const latinicDict = {
+  A: 'А', a: 'а',
+  B: 'Б', b: 'б',
+  C: 'Ц', c: 'ц',
+  Č: 'Ч', č: 'ч',
+  Ć: 'Ћ', ć: 'ћ',
+  D: 'Д', d: 'д',
+  Đ: 'Ђ', đ: 'ђ',
+  E: 'Е', e: 'е',
+  Ě: 'Ѣ', ě: 'ѣ',
+  F: 'Ф', f: 'ф',
+  G: 'Г', g: 'г',
+  H: 'Х', h: 'х',
+  I: 'И', i: 'и',
+  Y: 'Ы', y: 'ы',
+  J: 'Ј', j: 'ј',
+  K: 'К', k: 'к',
+  L: 'Л', l: 'л',
+  M: 'М', m: 'м',
+  N: 'Н', n: 'н',
+  O: 'О', o: 'о',
+  P: 'П', p: 'п',
+  R: 'Р', r: 'р',
+  S: 'С', s: 'с',
+  Ś: 'Сь', ś: 'сь',
+  Š: 'Ш', š: 'ш',
+  T: 'Т', t: 'т',
+  U: 'У', u: 'у',
+  V: 'В', v: 'в',
+  Z: 'З', z: 'з',
+  Ź: 'Зь', ź: 'зь',
+  Ž: 'Ж', ž: 'ж',
+}
 
-  .replace(/A/g, 'А')
-  .replace(/B/g, 'Б')
-  .replace(/C/g, 'Ц')
-  .replace(/Č/g, 'Ч')
-  .replace(/Ć/g, 'Ћ')
-  .replace(/D/g, 'Д')
-  .replace(/Đ/g, 'Ђ')
-  .replace(/E/g, 'Е')
-  .replace(/Ě/g, 'Ѣ')
-  .replace(/F/g, 'Ф')
-  .replace(/G/g, 'Г')
-  .replace(/H/g, 'Х')
-  .replace(/I/g, 'И')
-  .replace(/Y/g, 'Ы')
-  .replace(/J/g, 'Ј')
-  .replace(/K/g, 'К')
-  .replace(/L/g, 'Л')
-  .replace(/M/g, 'М')
-  .replace(/N/g, 'Н')
-  .replace(/O/g, 'О')
-  .replace(/P/g, 'П')
-  .replace(/R/g, 'Р')
-  .replace(/S/g, 'С')
-  .replace(/Ś/g, 'Сь')
-  .replace(/Š/g, 'Ш')
-  .replace(/T/g, 'Т')
-  .replace(/U/g, 'У')
-  .replace(/V/g, 'В')
-  .replace(/Z/g, 'З')
-  .replace(/Ź/g, 'Зь')
-  .replace(/Ž/g, 'Ж')
-
-const ocsTable = {
+const cyrillicDict = {
   A: 'A', a: 'a',
   Б: 'B', б: 'b',
   В: 'V', в: 'v',
@@ -128,7 +97,10 @@ const ocsTable = {
   // Ѵ: I, Y, V
   // ѵ: i, y, v
 }
-const toLatinic = text => [...text].map(x => ocsTable[x] || x).join('')
+
+const toLatinic = text => [...text].map(x => cyrillicDict[x] || x).join('')
+
+const toCyrillic = text => [...text].map(x => latinicDict[x] || x).join('')
 
 export default function transliterate(text = '', script, lang) {
   if (script === 'lat' && lang === 'ocs') return toLatinic(text)
